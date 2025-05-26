f289852d723da9020759b1370227cd31e66a516d
Srilaya S <srilaya.sangannagari@rxglobal.com>
2025-05-26 20:28:31 +0530

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
-import Html5Qr from './Html5Qr.jsx';
-import QRScanner from './QrScanner.jsx';
 import ReactQRScanner from "./ReactQrScanner.jsx";
+import Home from "./Home.jsx";
+import Html5Qr from "./Html5Qr.jsx";
+import QRScanner from "./QrScanner.jsx";
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <ReactQRScanner/>
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<Home/>} />
+                <Route path={"/html5-qr"} element={<Html5Qr />} />
+                <Route path={"/qr-scanner"} element={<QRScanner />} />
+                <Route path={"/react-qr-scanner"} element={<ReactQRScanner />} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => (
+    <div>
+        <h1>QR Code Scanners</h1>
+        <nav>
+            <ul>
+                <li><a href="/html5-qr">HTML5 QR</a></li>
+                <li><a href="/qr-scanner">QR Scanner</a></li>
+                <li><a href="/react-qr-scanner">React QR Scanner</a></li>
+            </ul>
+        </nav>
+    </div>
+)
+
+export default Home;
