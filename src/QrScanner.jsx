b2b05515596ecfa6678f6e3c2d94c5ca053eb3fc
Srilaya S <srilaya.sangannagari@rxglobal.com>
2025-05-28 14:56:00 +0530

    test custom zoom function

--- src/QrScanner.jsx
+++ src/QrScanner.jsx
@@ -1,5 +1,5 @@
 import QrScanner from "qr-scanner";
-import { useEffect, useRef, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 import Zoom from "./Zoom.jsx";
 
 const QRScanner = () => {
@@ -8,7 +8,10 @@ const QRScanner = () => {
     const [camera, setCamera] = useState("environment"); // State to manage camera selection
     const [isPaused, setIsPaused] = useState(false); // State to manage pause state
     const [qrScannerInstance, setQrScannerInstance] = useState(null); // State to manage the QR scanner instance
-    
+    const [capabilities, setCapabilities] = useState(null); // State to manage the QR scanner instance
+    const [settings, setSettings] = useState(null); // State to manage the QR scanner instance
+    const [videoTrack, setVideoTrack] = useState(null); // State to manage the QR scanner instance
+
     useEffect(() => {
         const videoElem = videoRef.current;
 
@@ -19,15 +22,30 @@ const QRScanner = () => {
                     console.log("Decoded QR code:", result);
                     setQrResult(result); // Update the state with the scanned result
                 },
-                { highlightScanRegion: true } // Optional: Highlight the scan region
+                {highlightScanRegion: true} // Optional: Highlight the scan region
             );
             qrScanner.turnFlashOn();
             qrScanner.start().catch((err) => console.error("Failed to start QR scanner:", err));
             qrScanner.setCamera("environment"); // Use the rear camera if available
             setQrScannerInstance(qrScanner); // Store the QR scanner instance in state
+            navigator.mediaDevices.getUserMedia({
+                audio: false,
+                video: true
+            }).then((stream) => {
+                console.log("Stream:", stream);
+                let vt = stream.getVideoTracks()[0];
+                console.log("cap:", vt.getCapabilities());
+                console.log("settings:", vt.getSettings());
+                setCapabilities(vt.getCapabilities());
+                setSettings(vt.getSettings());
+                setVideoTrack(vt);
+            }).catch((err) => {
+                console.error("Error accessing media devices:", err);
+            });
             return () => {
                 qrScanner.stop(); // Stop the scanner when the component unmounts
             };
+
         }
     }, []);
 
@@ -35,22 +53,21 @@ const QRScanner = () => {
         if (qrScannerInstance) {
             qrScannerInstance.setCamera(camera); // Set the camera based on the state
         }
-    },[qrScannerInstance,camera]);
+    }, [qrScannerInstance, camera]);
 
-    const ToggleCamera=()=>{
+    const ToggleCamera = () => {
         if (camera === "user") {
             setCamera("environment");
         } else {
             setCamera("user");
-        }   
+        }
     }
 
-    const PauseAndResumeCamera=()=>{
+    const PauseAndResumeCamera = () => {
         if (qrScannerInstance && !isPaused) {
             qrScannerInstance.$video.pause(); // Pause the video initially
             setIsPaused(true);
-        }
-        else if (qrScannerInstance && isPaused) {
+        } else if (qrScannerInstance && isPaused) {
             qrScannerInstance.$video.play();
             setIsPaused(false);
         }
@@ -62,14 +79,23 @@ const QRScanner = () => {
             <button onClick={ToggleCamera}>Toggle camera</button>
             <button onClick={PauseAndResumeCamera}>Pause camera</button>
 
-            <video ref={videoRef} style={{ width: "100%", maxHeight: "400px" }}></video>
+            <video ref={videoRef} style={{width: "100%", maxHeight: "400px"}}></video>
             {qrResult && (
                 <div>
                     <h2>Scanned QR Code:</h2>
                     <p>{qrResult.data}</p>
                 </div>
             )}
-            <Zoom></Zoom>
+            <Zoom
+                scanning={true}
+                capabilities={capabilities}
+                onZoom={(val) => {
+                    const newConstraints = {
+                        advanced: [{zoom: val}]
+                    };
+                    videoTrack.applyConstraints(newConstraints);
+                }}
+                value={settings}/>
         </div>
     );
 };
